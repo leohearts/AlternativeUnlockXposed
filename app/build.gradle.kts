4c9ff5232bd76f9e29db9512e3d1dc6ffcfb81cc
Leohearts <leohearts@leohearts.com>
2026-09-25 01:20:18 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.leohearts.alternativeUnlockHook"
         minSdk = 30
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.7"
+        versionCode = 10
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
